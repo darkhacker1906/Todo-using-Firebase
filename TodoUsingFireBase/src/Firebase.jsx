2e619be45b48cf7ca434387899a2572537d11d19
darkhacker1906 <singhabhinandan634@gmail.com>
2024-01-31 11:16:48 +0530

    Initial Setup

---
+++ TodoUsingFireBase/src/Firebase.jsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9aNcIXAzseBcUtz4mhnpWLJwwhD9Rf_A",
+  authDomain: "todo-using-firebase-bfe7f.firebaseapp.com",
+  projectId: "todo-using-firebase-bfe7f",
+  storageBucket: "todo-using-firebase-bfe7f.appspot.com",
+  messagingSenderId: "630626279961",
+  appId: "1:630626279961:web:ad7bad6ce5f84d21830088"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db=getFireStore(app);
